400c00b1f04cfddd24e226e8e260db81ffcfb228
kidmaiaok <pass0992203806@gmail.com>
2023-10-11 21:11:22 +0700

    ล่าสุดที่ให้อาจารดู
    no
    
    git commit -m ล่าสุดที่ให้อาจารดู

--- tset.py
+++ tset.py
@@ -1,14 +1,13 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import font
-import tkinter.messagebox
 import queue
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 # getting heuristics from file
 def getHeuristics():
     heuristics = {}
-    f = open("..\py\work3\\miniproject_python\heuristics.txt")
+    f = open("..\py\work3\miniproject_python\Treeheuristics.txt")
     for i in f.readlines():
         node_heuristic_val = i.split()
         heuristics[node_heuristic_val[0]] = int(node_heuristic_val[1])
@@ -17,25 +16,25 @@ def getHeuristics():
 
 
 # getting cities location from file
-def getCity():
-    city = {}
-    citiesCode = {}
-    f = open("..\py\work3\\miniproject_python\cities.txt")
+def gettree():
+    tree = {}
+    treeCode = {}
+    f = open("..\py\work3\miniproject_python\Trees.txt")
     j = 1
     for i in f.readlines():
-        node_city_val = i.split()
-        city[node_city_val[0]] = [int(node_city_val[1]), int(node_city_val[2])]
+        node_tree_val = i.split()
+        tree[node_tree_val[0]] = [int(node_tree_val[1]), int(node_tree_val[2])]
 
-        citiesCode[j] = node_city_val[0]
+        treeCode[j] = node_tree_val[0]
         j += 1
 
-    return city, citiesCode
+    return tree, treeCode
 
 
 # creating cities graph from file
 def createGraph():
     graph = {}
-    file = open("..\py\work3\miniproject_python\citiesGraph.txt")
+    file = open("..\py\work3\miniproject_python\TreesGraph.txt")
     for i in file.readlines():
         node_val = i.split()
 
@@ -70,7 +69,7 @@ def createGraph():
 
 
 # Greedy Best First Search Algorithm
-def GBFS(startNode, heuristics, graph, goalNode="Bucharest"):
+def GBFS(startNode, heuristics, graph, goalNode="Opium"):
     priorityQueue = queue.PriorityQueue()
     priorityQueue.put((heuristics[startNode], startNode))
 
@@ -93,83 +92,96 @@ def GBFS(startNode, heuristics, graph, goalNode="Bucharest"):
 
 
 # drawing map of answer
-def drawMap(city, gbfs, graph):
-    for i, j in city.items():
+def drawMap(tree, gbfs, graph):
+    for i, j in tree.items():
         plt.plot(j[0], j[1], "ro")
         plt.annotate(i, (j[0] + 5, j[1]))
 
         for k in graph[i]:
-            n = city[k[0]]
+            n = tree[k[0]]
             plt.plot([j[0], n[0]], [j[1], n[1]], "Gainsboro")
 
     for i in range(len(gbfs)):
         try:
-            first = city[gbfs[i]]
-            secend = city[gbfs[i + 1]]
+            first = tree[gbfs[i]]
+            secend = tree[gbfs[i + 1]]
 
             plt.plot([first[0], secend[0]], [first[1], secend[1]], "DarkGreen")
         except:
             continue
+    
+    plt.show()
 
 
 #GUI and running program
 def root():
     
     root = Tk()
-    root.title("AI Project")
-    root.geometry("1140x730+220+30")
-
+    root.title("Greedy Best First Search")
+    root.state('zoomed')
+    root.geometry("1160x730+220+30")
+    root.configure(bg='#CECECE')
+    
     heuristic = getHeuristics()
     graph = createGraph()
-    city, citiesCode = getCity()
-    fig,ax = plt.subplots()
+    tree, treeCode = gettree()
 
-    
-    frame = Frame(root)
-    
-
+    frame = Frame(root, bg="#CECECE")
     frame.pack()
 
-    frame_label_1 = LabelFrame(frame, border=0)
-    frame_label_1.grid(row=0, column=0, padx=20, pady=20)
+    frame_label_1 = LabelFrame(frame, bg="#3D3E3A", border=0)
+    frame_label_1.grid(row=0, column=0, padx=10, pady=10)
+
+    photo = PhotoImage(file = '..\py\work3\miniproject_python\pathmap.png')
+    label_img = Label(frame_label_1, image=photo, width=1127, height=546)
+    label_img.grid(row=0, column=0)
 
     for widget in frame_label_1.winfo_children():
-        widget.grid_configure(padx=10, pady=5)
+        widget.grid_configure(padx=1, pady=1)
 
-    frame_label_2 = LabelFrame(frame, border=0)
+    frame_label_2 = LabelFrame(frame, border=0, bg="#CECECE")
     frame_label_2.grid(row=1, column=0, padx=20, pady=10)
 
+    frame_label_24 = LabelFrame(frame_label_2, border=0)
+    frame_label_24.grid(row=0, column=0, padx=20, pady=10)
+
     dropdown_var = StringVar()
-    dropdown = ttk.Combobox(frame_label_2, textvariable=dropdown_var, values=['Select City', *city], width=53, state="readonly")
+    dropdown = ttk.Combobox(frame_label_2, textvariable=dropdown_var, values=['Select Tree', *tree], width=53, state="readonly")
     dropdown.current(0)
     dropdown.grid(row=0,column=0)
     
     def on_click():
         dropdown_1 = dropdown_var.get()
-        cityName = dropdown_1
-        if dropdown_var.get() != 'Select City': 
-            frame_label_3 = LabelFrame(frame_label_2, border=0)
-            frame_label_3.grid(row=1, column=0, padx=20, pady=10)
+        treeName = dropdown_1
+        if dropdown_var.get() != 'Select Tree' :
+            frame_label_3 = LabelFrame(frame, border=0, bg="#CECECE")
+            frame_label_3.grid(row=2, column=0, padx=10, pady=10)
             
-            label_start_txt = Label(frame_label_3, text="Starting in : " + dropdown_1, bg="white", width=47)
-            label_start_txt.grid(row=1,column=0)
+            label_start_txt = Label(frame_label_3, text="Starting in : " + dropdown_1, anchor="w",justify="left", bg="White", width=62)
+            label_start_txt.grid(row=1,column=0) 
 
-            label_end_txt = Label(frame_label_3, text = "Ending in : " + "Bucharest", bg="white", width=47)
+            label_end_txt = Label(frame_label_3, text = "Ending in : " + "Opium", anchor="w",justify="left", bg="White", width=62)
             label_end_txt.grid(row=2,column=0)
 
-            gbfs = GBFS(cityName, heuristic, graph)
-            label_print_gbfs = Label(frame_label_3, text=[*gbfs], bg="white", width=47)
+            gbfs = GBFS(treeName, heuristic, graph)
+            label_print_gbfs = Label(frame_label_3, text=['Output', '-->', *gbfs], anchor="w",justify="left", bg="White", width=62)
             label_print_gbfs.grid(row=3,column=0)
 
-            drawMap(city, gbfs, graph)
-            canvas =FigureCanvasTkAgg(fig,master=frame)
-            canvas.get_tk_widget().grid(row=0,column=0)
+            print("Tree Select => ", dropdown_1)
+            print("GBFS => ", gbfs)
 
             for widget in frame_label_3.winfo_children():
                 widget.grid_configure(padx=5, pady=5)
 
-    btn_1 = Button(frame_label_2, text="SUBMIT", command=on_click, width=10)
-    btn_1.grid(row=0,column=1)
+            plt.rcParams['figure.figsize'] = [10, 5]
+            drawMap(tree, gbfs, graph)
+
+
+    frame_label_25 = LabelFrame(frame_label_2, border=0)
+    frame_label_25.grid(row=0, column=1, padx=20, pady=10)
+
+    btn_1 = Button(frame_label_25, text="SUBMIT", command=on_click, width=10, bg="#01F9C6")
+    btn_1.grid(row=0,column=0)
 
     for widget in frame_label_2.winfo_children():
         widget.grid_configure(padx=10, pady=5)
